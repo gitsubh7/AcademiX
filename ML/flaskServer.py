ad5cd5e88db182f048295fc64fe99ef10f008e14
Abdul Subhan <abds020202@gmail.com>
2025-01-13 03:23:53 +0530

    add MongoDB connection setup in Flask server

--- ML/flaskServer.py
+++ ML/flaskServer.py
@@ -6,8 +6,24 @@ from PIL import Image
 from io import BytesIO
 port = 5000
 
+
+
+from pymongo import MongoClient
+
+
+uri = "mongodb+srv://subhaann77:<db_pass>@aggregationpipelines.1v1pq.mongodb.net/"
+
+client = MongoClient(uri)
+db = client["aggregationpipelines"]
+collection = db["academiX"]
+
+
+
 app=Flask(__name__)
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=port)
+
+client.close()
+
 
 
