4e6eb51b9db7d1a72f317e7c266e8c3b6a8ee9a3
Abdul Subhan <abds020202@gmail.com>
2025-01-13 02:16:31 +0530

    added multer middleware for handling file uploads over express

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
+import { DB_NAME } from "../constants.js"
 export const connectDB=async ()=>{
     try {
         const instance = await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`)
