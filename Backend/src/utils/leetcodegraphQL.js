ef4528f8f7a8f9747af24d69cccc2336501aa3ba
Abdul Subhan <abds020202@gmail.com>
2025-01-26 11:15:21 +0530

    refactor: simplify LeetCode GraphQL query by removing unnecessary fields

--- Backend/src/utils/leetcodegraphQL.js
+++ Backend/src/utils/leetcodegraphQL.js
@@ -1,38 +1,15 @@
 export const query = `
 query getUserProfile($username: String!) {
-  allQuestionsCount {
-    difficulty
-    count
-  }
   matchedUser(username: $username) {
-    contributions {
-      points
-    }
     profile {
-      reputation
       ranking
     }
-    submissionCalendar
     submitStats {
       acSubmissionNum {
-        difficulty
         count
-        submissions
-      }
-      totalSubmissionNum {
-        difficulty
-        count
-        submissions
       }
     }
   }
-  recentSubmissionList(username: $username) {
-    title
-    titleSlug
-    timestamp
-    statusDisplay
-    lang
-    __typename
-  }
+  
 }
 `;
