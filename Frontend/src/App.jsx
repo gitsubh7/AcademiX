5c334ec66c549aefb629a20cb28bc421fc2451f6
Abdul Subhan <abds020202@gmail.com>
2025-06-12 00:30:34 +0530

    made changes in frontend conflicting files

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from "./register/SignUp.jsx";
 import Home from "./home/Home.jsx";
 import ForgotPass from "./forgot-password/page.jsx";
 import ResetPass from "./passwordReset/page.jsx";
+import FaceLogin from "./face-login/Face-Login.jsx";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
       <Route path="/register" element={<SignUp />} />
       <Route path="/forgot-password" element={<ForgotPass />} />
       <Route path="/api/v1/student/passwordReset" element={<ResetPass />} />
+      <Route path="/face-login" element={<FaceLogin />} />
     </Routes>
   );  
 }
